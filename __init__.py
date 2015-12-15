316959eb200c1dd19cd4f06f59ed1cbd129a0700
adrien <adrien@arccoza.com>
2015-12-15 20:41:30 +0900

    Added RsyncChecksum, rsync rolling checksum.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,17 @@
 from pprint import pprint
 from six.moves import range
 from collections import deque
+# from numba import jit
+# import numpy
 
 
-class Rabin_Fingerprint(object):
+fn = lambda h, w, ps, p, v: ((h - (w[0] * ps)) * p) + v
+
+class RabinFingerprint(object):
 	def __init__(self, size, base=None):
 		self.size = size
 		self.window = deque(maxlen=size)
+		self.cur = len(self.window)
 		self.hash = None
 		self.base = base or 101#10007
 		self._maxfact = self.base**(self.size-1) #store the prime ^ size-1 value, so we don't have to calc it everytime.
@@ -14,7 +19,7 @@ class Rabin_Fingerprint(object):
 	@staticmethod
 	def calc(window, base=None):
 		w = window
-		n= len(window)
+		n = len(window)
 		p = base or 101
 		h = 0
 
@@ -27,34 +32,44 @@ class Rabin_Fingerprint(object):
 			# print(p**(n-i-1), v)
 		# print(h)
 		return h
-
-	def feed(self, items):
+		
+	def feed(self, items, is_str=False):
 		s = self.size
 		n = None
 		w = self.window
+		l = self.cur
 		p = self.base
 		ps = self._maxfact
 		h = 0
+		# is_str = isinstance(items, str)
 
 		# if isinstance(items, str):
 		# 	items = [ord(v) for v in items]
 
-		for i, v in enumerate(items):
-			l = len(w)
+		# for i, v in enumerate(items):
+		for v in items:
+			# l = len(w)
+			# print(l, i)
 			n = s - 1 - l
 			n = n if n > 0 else 0
-			try:
+			# try:
+			# 	v = ord(v)
+			# except TypeError as ex:
+			# 	pass
+			if is_str:
 				v = ord(v)
-			except TypeError as ex:
-				pass
 			if l == s:
 				h = ((h - (w[0] * ps)) * p) + v
+				# h = fn(h, w, ps, p, v)
 				w.append(v)
 			else:
 				h += (p**n) * v
+				# h += numpy.multiply(numpy.power(p,n), v)
 				w.append(v)
+				l += 1
 		# pprint(h)
 		# pprint(w)
+		self.cur = l
 		self.hash = h
 		return h
 
@@ -65,9 +80,77 @@ class Rabin_Fingerprint(object):
 		return len(self.window)
 
 
+class RsyncChecksum(object):
+	def __init__(self, size, base=None):
+		self.size = size
+		self.window = deque(maxlen=size)
+		self.cur = len(self.window)
+		# self.hashA = 0
+		self.ihash = 0 # Interim hash (A).
+		self.hash = 0 # Final hash (B).
+
+	@staticmethod
+	def calc(window):
+		w = window
+		n = len(window)
+		a = 0
+		b = 0
+
+		for i in range(0, n):
+			try:
+				v = ord(w[i])
+			except TypeError as ex:
+				v = w[i]
+			a += v
+			b += a
+		# print(b)
+		return b
+		
+	def feed(self, items, is_str=False):
+		s = self.size
+		w = self.window
+		l = self.cur
+		a = self.ihash
+		b = self.hash
+
+		for v in items:
+			if is_str:
+				v = ord(v)
+			if l == s:
+				# a -= w[0]
+				# a += v
+				a = a - w[0] + v
+				# b -= s * w[0]
+				# b += a
+				b = b - (s * w[0]) + a
+				w.append(v)
+			else:
+				a += v
+				b += a
+				w.append(v)
+				l += 1
+		# pprint(b)
+		# pprint(w)
+		self.cur = l
+		self.ihash = a
+		self.hash = b
+		# self.hash = b
+		return b
+
+	def __str__(self):
+		return str(self.hash)
+
+	def __len__(self):
+		return len(self.window)
+
+
 
 if __name__ == '__main__':
-	f = Rabin_Fingerprint(3)
-	Rabin_Fingerprint.calc('bra', 101)
-	f.feed('abra')
-	pprint(len(f))
+	# f = RabinFingerprint(3)
+	# RabinFingerprint.calc('bra', 101)
+	# f.feed('abr', is_str=True)
+	# pprint(len(f))
+	f = RsyncChecksum(3)
+	RsyncChecksum.calc('bra')
+	f.feed('abra', is_str=True)
+	# pprint(len(f))
--- test.py
+++ test.py
@@ -1,11 +1,42 @@
-from __init__ import Rabin_Fingerprint
+from __init__ import RabinFingerprint, RsyncChecksum
 import timeit
+import cProfile, pstats, StringIO
 
 
-f = Rabin_Fingerprint(3)
-f.feed('abr')
-def speed_test():
-	f.feed('a')
+pr = cProfile.Profile()
+
+rf = RabinFingerprint(3)
+# rf.feed('abr', is_str=True)
+rf.feed([97, 98, 114])
+def rabin_test():
+	# rf.feed('a', is_str=True)
+	rf.feed([97])
+	# ord('a')
+
+rc = RsyncChecksum(3)
+# rc.feed('abr', is_str=True)
+rc.feed([97, 98, 114])
+def rsync_test():
+	# rc.feed('a', is_str=True)
+	rc.feed([97])
 
 if __name__ == '__main__':
-	print(timeit.timeit(stmt='speed_test()', setup='from __main__ import speed_test', number=10000000))
+	# pr.enable()
+	# print(timeit.timeit(stmt='rabin_test()', setup='from __main__ import rabin_test', number=1000000))
+	# pr.disable()
+	# s = StringIO.StringIO()
+	# sortby = 'cumulative'
+	# ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+	# ps.print_stats()
+	# print s.getvalue()
+	# cProfile.run('rabin_test()')
+	# print(timeit.timeit('"h = (((999509 - (97 * 10201)) * 101) + 97)"', number=10000000))
+
+	pr.enable()
+	print(timeit.timeit(stmt='rsync_test()', setup='from __main__ import rsync_test', number=1000000))
+	pr.disable()
+	s = StringIO.StringIO()
+	sortby = 'cumulative'
+	ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+	ps.print_stats()
+	print s.getvalue()
